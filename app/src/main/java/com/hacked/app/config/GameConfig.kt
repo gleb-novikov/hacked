bff3e1492c43db009b2aab7696ba6c60c82a2ffb
gleb-novikov <novikovglebka@gmail.com>
2022-11-12 18:52:45 +0300

    feat: add poi & geodesy

---
+++ app/src/main/java/com/hacked/app/config/GameConfig.kt
@@ -0,0 +1,9 @@
+package com.hacked.app.config
+
+object GameConfig {
+    /** Дистанция, на которой игрок может взаимодействовать с объектами на карте (в метрах).  */
+    var distanceInteraction = 100
+
+    /** Время деактивации точки после взлома (в секундах).  */
+    var timeDeactivation = 3 * 60 * 60
+}
